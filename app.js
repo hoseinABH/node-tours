8f551dd0e0cc3708268df1cf758385be7a626a0f
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-03-24 19:29:53 +0430

    feat:add delete and update route

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const fs = require('fs');
+
+// Initilize app
 const app = express();
 
 // Middleware For Getting Request Body
 app.use(express.json());
 
+// Fake Database
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+// Handlers
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'successful',
     results: tours.length,
@@ -17,13 +21,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTourById = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failure',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'successful',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-
   const newTour = Object.assign({ id: newId }, req.body);
-
   tours.push(newTour);
 
   fs.writeFile(
@@ -38,10 +57,41 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  const id = +req.params.id;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failure',
+      message: 'Invalid ID',
+    });
+  }
+};
+
+const deleteTour = (req, res) => {
+  const id = +req.params.id;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failure',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'successful',
+    message: 'Deleted',
+  });
+};
+
+// Routes
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTourById);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`App Running on port: ${port}`);
 });
