d9c8c9a2dd117614f381597bf5899df3bb307427
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-03-27 22:28:02 +0430

    feat:implement update query in update tour handler

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -53,11 +53,22 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Updated',
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const newTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: newTour,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
