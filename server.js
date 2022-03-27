e6c047ed53a4a5208fb23c7089fd921317a60682
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-03-27 13:45:59 +0430

    feat:add mongoose and create the first doc

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true,
+  },
+  rate: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price.'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'Green land',
+  price: 200,
+  rate: 4.8,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc, 'Doc Saved.');
+  })
+  .catch((err) => {
+    console.log('ERROR!', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
